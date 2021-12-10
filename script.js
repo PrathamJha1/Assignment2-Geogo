26436b2c5cea77e8b3ebfb95fe15e6c5e57733e4
Pratham Jha <prathamjha5683@gmail.com>
2021-12-10 21:08:27 +0530

    All Api Calling Trial Done

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let city= "New York";
-let unit= "C";
+let unit='C';
 let Day="Today";
-console.log(unit);
-unit=$('.switch').innerHTML;
-document.getElementById('Submit').addEventListener('click',function(){
+document.getElementById('c').addEventListener('click',function(){  
+    document.getElementById('c').style.backgroundColor='#b71c1c';
+    document.getElementById('f').style.backgroundColor='white';
+    unit=$('#c')[0].name;
+    event.preventDefault();
+})
+document.getElementById('f').addEventListener('click',function(){
+    event.preventDefault();
+    document.getElementById('f').style.backgroundColor='#b71c1c';
+    document.getElementById('c').style.backgroundColor='white';
+    unit=$('#f')[0].name;
+})
+    document.getElementById('Submit').addEventListener('click',function(){
     event.preventDefault();
     city=document.getElementById('Search').value;
     console.log(city);
-    $('#Search').html="";
-    console.log('hello');
+    $('#Search')[0].value="";
+    Location.fetchLocation(city);
+    weather.fetchWeather(lat,lon);
 });
+
 let lat="22.5726723",lon="88.3638815";
     let Location={
         apiKey:"6c65d5c02dc08e8e70fa959a377ba5bd",
@@ -31,9 +43,7 @@ document.getElementById('Curr_week').addEventListener('click',function(){
 });
 let weather={
     apiKey:"6c65d5c02dc08e8e70fa959a377ba5bd",
-    fetchWeather: function(){
+    fetchWeather: function(lat,lon){
         fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+this.apiKey).then((response)=>response.json()).then((data)=>console.log(data));
     }
 };
-
-$('span').html(unit);
